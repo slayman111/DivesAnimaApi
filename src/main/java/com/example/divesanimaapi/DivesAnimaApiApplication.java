ebf5aaef15b9879d1c23b0d82f4edd7b0e0a0805
Slayman11 <slayman111@mail.ru>
2023-09-25 21:24:10 +0300

    init

---
+++ src/main/java/com/example/divesanimaapi/DivesAnimaApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.divesanimaapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivesAnimaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivesAnimaApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/divesanimaapi/DivesAnimaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.divesanimaapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivesAnimaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
